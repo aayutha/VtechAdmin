5337348f740465e089c8e24f18feaab53ed0411a
aayutha <thatheraaayushi@gmail.com>
2022-11-28 17:10:19 +0530

    courselist

--- src/Screens/Course/Course.js
+++ src/Screens/Course/Course.js
@@ -1,8 +1,34 @@
 import React from "react";
 import { Box, Button, TextField, Typography } from "@mui/material";
-
+import { styled } from '@mui/material/styles';
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import ListItemAvatar from '@mui/material/ListItemAvatar';
+import ListItemIcon from '@mui/material/ListItemIcon';
+import ListItemText from '@mui/material/ListItemText';
+import Avatar from '@mui/material/Avatar';
+import IconButton from '@mui/material/IconButton';
+import FormGroup from '@mui/material/FormGroup';
+import FormControlLabel from '@mui/material/FormControlLabel';
 import Checkbox from "@material-ui/core/Checkbox";
+import Grid from '@mui/material/Grid';
+import FolderIcon from '@mui/icons-material/Folder';
+import DeleteIcon from '@mui/icons-material/Delete';
+
+function generate(element) {
+  return [0, 1, 2].map((value) =>
+    React.cloneElement(element, {
+      key: value,
+    }),
+  );
+}
+const Demo = styled('div')(({ theme }) => ({
+  backgroundColor: theme.palette.background.paper,
+}));
 const Course = () => {
+  const [dense, setDense] = React.useState(false);
+  const [secondary, setSecondary] = React.useState(false);
+
   return (
     <div>
       <form>
@@ -28,6 +54,43 @@ const Course = () => {
           <TextField sx={{ width: 300 }} margin="normal" type={'text'} variant="outlined" placeholder="Course Name" />
           <TextField sx={{ width: 300 }} margin="normal" type={'text'} variant="outlined" placeholder="Course Detail" />
           <TextField sx={{ width: 300 }} margin="normal" type={'text'} variant="outlined" placeholder="Course Price" />
+{/* mui list code */}
+
+
+      <Grid container spacing={2} alignContent="center">
+      
+        <Grid item xs={12} md={6}  >
+          <Typography sx={{ mt: 4, mb: 2 ,}} variant="h6" component="div" marginLeft={7}>
+            Course List
+          </Typography>
+          <Demo sx={{ m: 1}}>
+            <List dense={dense} >
+              {generate(
+                <ListItem sx={{ m: 3,width: 300 }}
+                  secondaryAction={
+                    <IconButton edge="end" aria-label="delete">
+                      <DeleteIcon />
+                    </IconButton>
+                  }
+                >
+                  <ListItemAvatar   >
+                    <Avatar>
+                      <FolderIcon />
+                    </Avatar>
+                  </ListItemAvatar>
+                  <ListItemText
+                    primary="Digital-Marketing"
+                    secondary={secondary ? 'Secondary text' : null}
+                  />
+                </ListItem>,
+              )}
+            </List>
+          </Demo>
+        </Grid>
+      </Grid>
+
+{/* mui list code */}
+         
 
           <Button
             sx={{ marginTop: 3, borderRadius: 3, width: 220 }}
